4fd27be7e68e7821fe48729ad7527b51f131f957
JT Giri <jgiri@nclouds.com>
2016-09-13 16:52:09 -0700

    make it chef 11 compatible

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'ananda.manoharan@nclouds.com'
 license 'all_rights'
 description 'Installs/Configures jenkins_config'
 long_description 'Installs/Configures jenkins_config'
-version '1.0.1'
+version '1.0.2'
 
 depends 'jenkins', '~> 2.5.0'
 depends 'java', '~> 1.39.0'
 
 
-source_url 'https://github.com/nclouds/jenkins_config.git'
-issues_url 'https://github.com/nclouds/jenkins_config/issues'
+source_url 'https://github.com/nclouds/jenkins_config.git' if respond_to?(:source_url)
+issues_url 'https://github.com/nclouds/jenkins_config/issues' if respond_to?(:issues_url)
